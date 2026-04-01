cc1e770819e75330e74ac4ec18166d9008488056
yayinobody <arjay.alanano23@gmail.com>
2026-04-01 20:30:02 +0800

    feat(Routes): add crud routes for students, users, and faculties

--- routes/web/students.php
+++ routes/web/students.php
@@ -5,11 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
-    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
-    Route::post('/students/store/', [StudentController::class, 'store'])->name('students.store');
-    Route::get('/students/profile/{student}', [StudentController::class, 'show'])->name('students.show');
-    Route::get('/students/subjects/{student}', [StudentController::class, 'showSubjects'])->name('students.showSubjects');
-    Route::put('/students/update/{student}', [StudentController::class, 'update'])->name('students.update');
-    Route::delete('/students/destroy/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
+    Route::get('/students', [StudentController::class, 'index'])->name('students');
+    Route::post('/students/store/', [StudentController::class, 'store'])->name('storeStudent');
+    Route::get('/students/profile/{student}', [StudentController::class, 'show'])->name('showStudent');
+    Route::get('/students/subjects/{student}', [StudentController::class, 'showSubjects'])->name('showStudentSubjects');
+    Route::put('/students/update/{student}', [StudentController::class, 'update'])->name('updateStudent');
+    Route::delete('/students/destroy/{student}', [StudentController::class, 'destroy'])->name('deleteStudent');
 
 });
--- routes/web/users.php
+++ routes/web/users.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/users', [UserController::class, 'index'])->name('users');
-    Route::post('/users/store',[UserController::class, 'store'])->name('users.store');
-    Route::get('/users/show/{user}',[UserController::class, 'show'])->name('users.show');
+    Route::post('/users/store',[UserController::class, 'store'])->name('storeUser');
+    Route::get('/users/show/{user}',[UserController::class, 'show'])->name('showUser');
+    Route::delete('/users/delete/{user}', [UserController::class,'destroy'])->name('deleteUser');
+
 
 });
