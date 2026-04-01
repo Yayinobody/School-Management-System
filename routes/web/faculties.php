e939dd26c6abb34ab65ca462f907ec8092183019
yayinobody <arjay.alanano23@gmail.com>
2026-04-01 19:10:25 +0800

    fix(Routes): reanamed faculty route name

--- routes/web/faculties.php
+++ routes/web/faculties.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/faculties', [FacultyController::class, 'index'])->name('faculties');
-    Route::get('/faculties/show/{faculty}',[FacultyController::class, 'show'])->name('faculties.showFaculty');
+    Route::get('/faculties/show/{faculty}',[FacultyController::class, 'show'])->name('faculties.show');
 });
