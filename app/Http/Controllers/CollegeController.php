a3284611b27f4678b6ed8d9db42c29c7b4308a28
yayinobody <arjay.alanano23@gmail.com>
2026-04-02 22:15:41 +0800

    feat(CollegeController): add index function

--- app/Http/Controllers/CollegeController.php
+++ app/Http/Controllers/CollegeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\College;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreCollegeRequest;
 
 class CollegeController extends Controller
 {
@@ -12,27 +13,41 @@ class CollegeController extends Controller
      */
     public function index()
     {
-        $colleges = College::with('programs:id,faculties','rooms')->get();
+        $colleges = College::with('rooms','programs.faculties')->get();
         $college_data = $colleges->map(
         function ($college){
             return[
                 "code" => $college->code,
                 "title" => $college->title,
-                "total_programs" => $college->programs->count();
-                "total_rooms" => $college->rooms->count()
+                "total_programs" => $college->programs->count(),
+                "total_rooms" => $college->rooms->count(),
+                "total_faculties" => $college->programs->sum(function ($program) {
+                                return $program->faculties->count();,
             ];
         });
 
-
+        return inertia('college'[
+            "data" => $college_data
+        ]);
 
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCollegeRequest $request)
     {
-        //
+        $validated = $request->validated();
+
+        $college = College::create($validated);
+
+        return response()->json(
+        [
+            "message" => "College created Succesfully",
+            "data" => $college
+        ],200
+        );
+
     }
 
     /**
