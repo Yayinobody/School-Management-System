57c28a2abedd72140cd584d1584909eec786f080
yayinobody <arjay.alanano23@gmail.com>
2026-04-02 20:39:03 +0800

    Create CollegeController.php

---
+++ app/Http/Controllers/CollegeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\College;
+use Illuminate\Http\Request;
+
+class CollegeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $colleges = College::with('programs:id,faculties','rooms')->get();
+        $college_data = $colleges->map(
+        function ($college){
+            return[
+                "code" => $college->code,
+                "title" => $college->title,
+                "total_programs" => $college->programs->count();
+                "total_rooms" => $college->rooms->count()
+            ];
+        });
+
+
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(College $college)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, College $college)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(College $college)
+    {
+        //
+    }
+}
